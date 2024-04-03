4defd3ade5674fc248488a6dc36469d1bd8a78ee
LaChaznie Moore <lachazniemoore@LaChaznies-MacBook-Air.local>
2024-04-02 22:43:30 -0400

    updates to the design, fixed somethings

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 function getAnswer() {
-    var question = getElementById("questionInput").value;
-    if (question.trim()=== ""){
+    var question = document.getElementById("questionInput").value;
+    if (question.trim() === "") {
         alert("Do you have a question???");
         return;
-
     }
 
     var answers = ["Absolutely", "Of course, love!", "Anything for you!", "Definitely", "Duh!", "Miracles happen everyday", "Without a doubt!",
-                    "Absolutely Not!", "Of course not", "Your're Joking right?", "Nah...", "Sorry to disappoint you...", "It's looking like a no", "The delusion is strong herer",
+                    "Absolutely Not!", "Of course not", "You're Joking right?", "Nah...", "Sorry to disappoint you...", "It's looking like a no", "The delusion is strong here",
                     "It's not looking good for you Chief", "Nope", "Ehh...", "A strong possibility", "Idk.. Maybe", "Not saying no, but can't say yes", "Anything is possible",
-                    "Well I guess so...", "Let me think about it", "Sorry, I was admiring my beauty", "Taking a twerk break, Try again later.", "i gotta see..", "Oops!!! I wasn't listening",
+                    "Well I guess so...", "Let me think about it", "Sorry, I was admiring my beauty", "Taking a twerk break, Try again later.", "I gotta see..", "Oops!!! I wasn't listening",
                     "My bad, What did you say?"];
     
-    var randomIndex = Math.floor(Math.random() *answer.length);
-    var answers = answers[randomIndex];
+    var randomIndex = Math.floor(Math.random() * answers.length);
+    var selectedAnswer = answers[randomIndex];
 
-    document.getElementById("answerDisplay").innerHTML = "<strong>Question:</strong> " + question + "<br><strong>Answer:</strong> " + answer;
-
+    document.getElementById("answerDisplay").innerHTML = "<strong>Question:</strong> " + question + "<br><strong>Answer:</strong> " + selectedAnswer;
 }
